6c4167c3b5e31c6a95fe3c1d5a532b9d436c125a
Santiago Silva <egs.santi.silva@gmail.com>
2026-06-03 15:33:12 -0600

    python ex 1: test

--- practice-scripts/ec2_lister/create_ec2.sh
+++ practice-scripts/ec2_lister/create_ec2.sh
@@ -1,10 +1,19 @@
 # Launch a t2.micro (free tier) in your console OR via CLI:
+
+# for linux/macOS
 aws ec2 run-instances \
-  --image-id ami-0c02fb55956c7d316 \
-  --instance-type t2.micro \
+  --image-id ami-0f3caa1cf4417e51b \
+  --instance-type t3.micro \
   --count 1 \
   --tag-specifications \
     'ResourceType=instance,Tags=[{Key=Environment,Value=dev},{Key=App,Value=myapp}]'
+
+# for Windows
+aws ec2 run-instances `
+  --image-id ami-0f3caa1cf4417e51b `
+  --instance-type t3.micro `
+  --count 1 `
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Environment,Value=dev},{Key=App,Value=myapp},{Key=Name,Value=SecondPython}]"
 
 # Note the InstanceId in the output — you'll see it in your script
 # STOP it after testing (not terminate) to avoid charges:
--- practice-scripts/ec2_lister/test.sh
+++ practice-scripts/ec2_lister/test.sh
@@ -1,6 +1,17 @@
 # All instances tagged Environment=dev
 python ec2_lister.py --tag-key Environment --tag-value dev
 
+# Output:
+# (.venv) PS D:\HCI_Academy\python-devops\practice-scripts\ec2_lister> python ec2_lister.py --region us-east-1
+#                                         EC2 instances — us-east-1                                        
+# ┏━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓
+# ┃ ID                  ┃ Name         ┃ Type     ┃ State   ┃ AZ         ┃ Private IP    ┃ Public IP      ┃
+# ┡━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━┩
+# │ i-0a7ed0f26c6a01d56 │ SecondPython │ t3.micro │ running │ us-east-1d │ 172.31.41.16  │ 54.196.123.163 │
+# │ i-0ed20b7a96ee29cd3 │ FirstPython  │ t3.micro │ stopped │ us-east-1d │ 172.31.32.188 │ —              │
+# └─────────────────────┴──────────────┴──────────┴─────────┴────────────┴───────────────┴────────────────┘
+# Total: 2 instance(s)
+
 # Only running ones
 python ec2_lister.py --tag-key Environment --tag-value dev --state running
 
